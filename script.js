da4f43208637f0e911d86ad7c7c4b0d979e72dd6
zahadneokurkycz <filip.belzik@email.cz>
2023-01-29 12:24:17 +0100

    Update files

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 var pole = location.search.substring(1).split('&'); 
-
 var copyarea = document.getElementById('copyarea');
-
 var copybtn = document.getElementById('copybtn')
-
 var httpGetVars = new Array(); 
 
-
-
 for(i = 0; i < pole.length; i++){
-
 	httpGetVars[pole[i].split('=')[0]] = decodeURIComponent(pole[i].split('=')[1]); 
-
 }
 
 document.documentElement.setAttribute('data-bs-theme', httpGetVars['theme']);
+copyarea.value = httpGetVars['text'];
 
-copyarea.innerHTML = httpGetVars['text'];
-
-
-
-function copy() {
-
-    copybtn.innerHTML = '<i class="bi bi-clipboard-check-fill"></i>';
-
-    copyarea.select();
-
-    copyarea.setSelectionRange(0, 99999);
-
-    navigator.clipboard.writeText(copyarea.innerHTML);
-
+document.getElementById("copybtn")
+.onclick = function() {
+    let text = document.getElementById("copyarea").value;
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        alert('Text copied to clipboard');
+    })
+    .catch(err => {
+        alert('Error in copying text: ', err);
+    });
 }
