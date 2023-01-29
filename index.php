b977df2d30d7d09e221db4fa52f62b0de9300b76
zahadneokurkycz <filip.belzik@email.cz>
2023-01-29 14:16:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-        <script src="script.js"></script>
+        <!--<script src="script.js"></script>-->
         <meta charset="utf-8">
         <title>Copy system</title>
         <meta name="viewport" content="width=device-width,initial-scale=1">
     </head>
     <body>
-        <textarea id="copyarea" title="Text to copy"><?php
-            echo $_GET['text']; 
-         ?></textarea>
-        <span id="copyspan" style="display: none;"></span>
-        <button class="btn btn-outline-success" id="copybtn" title="Copy"><i class="bi bi-clipboard"></i></button>
+        <script>
+            navigator.permissions.query({ name: "write-on-clipboard" }).then((result) => {
+                if (result.state == "granted" || result.state == "prompt") {
+                    alert("Write access granted!");
+                }
+            });
+        </script>
     </body>
 </html>
