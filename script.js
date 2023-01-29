b8cb755d973b319e09b04d7a59c3504835723641
zahadneokurkycz <filip.belzik@email.cz>
2023-01-29 12:43:37 +0100

    SUSSY BAKA

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var pole = location.search.substring(1).split('&'); 
 var copyarea = document.getElementById('copyarea');
-var copybtn = document.getElementById('copybtn')
+var copybtn = document.getElementById('copybtn');
+var copyspan = document.getElementById('copyspan');
 var httpGetVars = new Array(); 
 
 for(i = 0; i < pole.length; i++){
@@ -8,7 +9,8 @@ for(i = 0; i < pole.length; i++){
 }
 
 document.documentElement.setAttribute('data-bs-theme', httpGetVars['theme']);
-copyarea.value = httpGetVars['text'];
+copyspan.innerHTML = httpGetVars['text'];
+copyarea.value = copyspan.innerHTML;
 
 document.getElementById("copybtn")
 .onclick = function() {
